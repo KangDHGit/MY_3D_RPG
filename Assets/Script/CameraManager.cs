7304ef62c73a439abc16f87892ed3b4da38b19bb
KangDHGit <eogks0323@gmail.com>
2022-11-29 00:28:28 +0900

    오브젝트 투명화 작업중

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraManager : MonoBehaviour
 {
     public static CameraManager I;
     Vector3 _camRot = new Vector3(30, -45);
     Vector3 _camPos = new Vector3(52.5f, 44.0f, -52.5f);
+
+    Renderer ObstacleRenderer;
     private void Awake()
     {
         I = this;
@@ -16,5 +19,29 @@ public class CameraManager : MonoBehaviour
     void Update()
     {
         transform.position = Player.I.transform.position + _camPos;
+
+        float Distance = Vector3.Distance(transform.position, Player.I.transform.position);
+
+        Vector3 Direction = (Player.I.transform.position - transform.position).normalized;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Direction, out hit, Distance))
+        {
+            // 2.맞았으면 Renderer를 얻어온다.
+            if (hit.transform.tag == "Object")
+            {
+                ObstacleRenderer = hit.transform.gameObject.GetComponentInChildren<Renderer>();
+
+                if (ObstacleRenderer != null)
+                {
+                    // 3. Metrial의 Aplha를 바꾼다.
+                    Material Mat = ObstacleRenderer.material;
+                    Color matColor = Mat.color;
+                    matColor.a = 0.25f;
+                    Mat.color = matColor;
+                }
+            }
+        }
     }
 }
